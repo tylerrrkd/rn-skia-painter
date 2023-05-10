c957719455bf93b31f60489ccbfcb6f634bbf058
Tylerrrkd <kaida23@126.com>
2023-05-11 01:10:34 +0800

    :sparkles: stack with setting

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,12 +1,15 @@
 import { HomeScreen } from 'app/features/home/screen'
 import { Stack } from 'expo-router'
+import { Button } from '@my/ui'
+import { Settings } from '@tamagui/lucide-icons'
 
 export default function Screen() {
   return (
     <>
       <Stack.Screen
         options={{
-          title: 'Home',
+          title: '',
+          headerLeft: () => <Button icon={Settings} chromeless />,
         }}
       />
       <HomeScreen />
