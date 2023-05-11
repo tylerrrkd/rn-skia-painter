859040d10f5ce02dbb799513371a810ba5ac8877
Tylerrrkd <kaida23@126.com>
2023-05-12 01:12:06 +0800

    :sparkles: basically structure

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,15 +1,25 @@
 import { HomeScreen } from 'app/features/home/screen'
 import { Stack } from 'expo-router'
-import { Button } from '@my/ui'
+import { ButtonText } from '@my/ui'
 import { Settings } from '@tamagui/lucide-icons'
+import { useMemo } from 'react'
 
 export default function Screen() {
+  const isConnected = false
+
+  const disabled = useMemo(() => !isConnected, [isConnected])
+
   return (
     <>
       <Stack.Screen
         options={{
           title: '',
-          headerLeft: () => <Button icon={Settings} chromeless />,
+          headerLeft: () => (
+            <ButtonText {...{ disabled }}>
+              <Settings />
+            </ButtonText>
+          ),
+          headerRight: () => <ButtonText {...{ disabled }}>未连接</ButtonText>,
         }}
       />
       <HomeScreen />
