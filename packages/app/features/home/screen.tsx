5ef9dc0f646ca6e116b7a8a483b045712faab2c1
Tylerrrkd <kaida23@126.com>
2023-05-16 22:41:47 +0800

    :sparkles: with localization

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -2,18 +2,20 @@ import { Button, Text, XStack, YStack } from '@my/ui'
 // import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTranslation } from '@my/locales'
 
 export function HomeScreen() {
   const { bottom } = useSafeAreaInsets()
+  const { t } = useTranslation()
 
   return (
     <YStack f={1} p="$4" space>
       <YStack f={1} jc="center" ai="center">
-        <Button circular>开始</Button>
+        <Button circular>{t('start')}</Button>
       </YStack>
       <YStack bottom={bottom}>
         <XStack>
-          <Button f={1}>文件列表</Button>
+          <Button f={1}>{t('file list')}</Button>
         </XStack>
       </YStack>
     </YStack>
