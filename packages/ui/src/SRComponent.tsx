a73dfb53c72650ce281c186bc3a917065fb7a765
Tylerrrkd <kaida23@126.com>
2023-05-21 11:19:54 +0800

    💄 add SRScreen

--- packages/ui/src/SRComponent.tsx
+++ packages/ui/src/SRComponent.tsx
@@ -1,4 +1,20 @@
 import { Button, styled, YStack } from 'tamagui'
+import { Stack } from 'expo-router'
+
+export const SRScreen: React.FC<React.ComponentProps<typeof Stack.Screen>> = (props) => (
+  <Stack.Screen
+    {...props}
+    options={{
+      headerTintColor: "black",
+      headerShadowVisible: false,
+      contentStyle: {
+        borderTopColor: '#F78F21',
+        borderTopWidth: 1,
+      },
+      ...props.options,
+    }}
+  />
+)
 
 export const SRYStack = styled(YStack, {
   name: 'SRYStack',
