3066a62738be8a2e2fcec779c29b517e8d24884b
Tylerrrkd <kaida23@126.com>
2023-06-01 18:13:01 +0800

    💄 dark mode status bar

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -2,6 +2,7 @@ import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Provider } from 'app/provider'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 export default () => {
   const [loaded] = useFonts({
@@ -15,6 +16,7 @@ export default () => {
   return (
     <Provider>
       <ThemeProvider value={DefaultTheme}>
+        <StatusBar style="dark" />
         <Stack />
       </ThemeProvider>
     </Provider>
