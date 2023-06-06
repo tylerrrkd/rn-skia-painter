34c87c06780b362758d8d9a0cf91d79c37541ff9
Tylerrrkd <kaida23@126.com>
2023-06-07 01:36:43 +0800

    🐛 default

--- packages/locales/src/config/languages.ts
+++ packages/locales/src/config/languages.ts
@@ -9,8 +9,8 @@ export const languages: Record<LanguageCode, Language> = {
 }
 
 export const locales: Record<LanguageCode, Record<string, string>> = {
-  [LanguageCode.en]: require('./en-US'),
-  [LanguageCode.zh]: require('./zh-CN'),
+  [LanguageCode.en]: require('./en-US').default,
+  [LanguageCode.zh]: require('./zh-CN').default,
 }
 
 const languageList = Object.values(languages)
