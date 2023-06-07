b3152e2adb9b422ced59ed72d291d2e2ace78312
Tylerrrkd <kaida23@126.com>
2023-06-07 22:11:04 +0800

    💄 update inactive color

--- packages/ui/src/theme/tokens.ts
+++ packages/ui/src/theme/tokens.ts
@@ -131,7 +131,7 @@ export const commonColors = {
   primary: '#F78F21',
   primaryPressBackground: 'rgba(247,143,33,0.6)',
   border: '#989898',
-  inactive: '#ddd',
+  inactive: '#c5c5c5',
 }
 
 export const darkColors = {
