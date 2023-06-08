23156ab8d97f0a6f9fbbdcf1e56448f148c2ae90
Tylerrrkd <kaida23@126.com>
2023-06-09 00:55:54 +0800

    ✨init with store and complete ux

--- packages/ui/src/components/SRConnectIPDialog.tsx
+++ packages/ui/src/components/SRConnectIPDialog.tsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { GetProps, Input, XStack } from 'tamagui'
 import { useTranslation } from '@my/locales'
+import { useSettingStore } from '@my/stores'
 import { SRDialog } from './SRDialog'
 
 export const SRConnectIPDialog: React.FC<GetProps<typeof SRDialog>> = ({ ...props }) => {
   const { t } = useTranslation()
-  const [IPAddress, setIPAddress] = useState('')
+  const [IPAddress, setIPAddress] = useSettingStore((state) => [
+    state.IPAddress,
+    state.setIPAddress,
+  ])
+  const [typingIPAddress, setTypingIPAddress] = useState(IPAddress)
+  const [isError, setIsError] = useState(false)
+
+  const onConfirm = useCallback(async () => {
+    if (!typingIPAddress) {
+      setIsError(true)
+      return false
+    } else if (!/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(typingIPAddress)) {
+      setIsError(true)
+      return false
+    }
+    setIsError(false)
+    setIPAddress(typingIPAddress)
+    return true
+  }, [typingIPAddress])
 
   return (
     <SRDialog
       tittle={t('connect IP address')}
+      onConfirm={onConfirm}
       content={
         <XStack>
           <Input
@@ -17,16 +37,23 @@ export const SRConnectIPDialog: React.FC<GetProps<typeof SRDialog>> = ({ ...prop
             textAlign="center"
             borderWidth={'$0.5'}
             borderStyle={'solid'}
-            borderColor={'$border'}
-            focusStyle={{
-              borderWidth: '$0.5',
-              borderColor: '$border',
-            }}
+            borderColor={isError ? 'red' : '$border'}
+            focusStyle={
+              isError
+                ? {}
+                : {
+                    borderWidth: '$0.5',
+                    borderColor: '$border',
+                  }
+            }
             keyboardType="numeric"
-            value={IPAddress}
+            value={typingIPAddress}
             placeholder={t('enter IP address on the motherboard')}
             placeholderTextColor={'$gray11'}
-            onChange={(event) => setIPAddress(event?.nativeEvent?.text)}
+            onChange={(event) => {
+              setIsError(false)
+              setTypingIPAddress(event?.nativeEvent?.text)
+            }}
           />
         </XStack>
       }
