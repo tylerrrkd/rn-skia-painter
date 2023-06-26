33752f1db41421c0cd8266b904b11ba6a9acdc89
Tylerrrkd <kaida23@126.com>
2023-06-27 01:19:10 +0800

    ✨ unified carve scene

--- packages/app/features/carve/scene.tsx
+++ packages/app/features/carve/scene.tsx
@@ -1,134 +1,29 @@
-import React, { cloneElement, useRef } from 'react'
+import React, { useRef } from 'react'
 import { useTranslation } from '@my/locales'
-import {
-  Checkbox,
-  GetProps,
-  Label,
-  SRIconButton,
-  ScrollView,
-  Slider,
-  SpaceTokens,
-  Text,
-  XStack,
-  YStack,
-} from '@my/ui'
-import {
-  Layers,
-  Type,
-  Brush,
-  Image,
-  View,
-  Contrast,
-  Droplet,
-  Lasso,
-  Palette,
-  Square,
-  MenuSquare,
-  Undo2,
-  Check,
-} from '@tamagui/lucide-icons'
+import { ScrollView, SpaceTokens, XStack, YStack } from '@my/ui'
+import { Layers, Type, Brush, Image, View } from '@tamagui/lucide-icons'
 import { DrawingBoard, DrawingBoardRef } from '@my/skia'
-
-const statusTextPxSpace: SpaceTokens = '$2'
-const ActionStatus = () => {
-  const { t } = useTranslation()
-
-  return (
-    <XStack
-      px={pxSpace}
-      height={'$3'}
-      alignItems="center"
-      justifyContent="space-between"
-      space="$2"
-    >
-      <Text fontSize="$3" space={statusTextPxSpace}>
-        {t('width')}
-        <Text>100</Text>mm
-        <Text>|</Text>
-        {t('height')}
-        <Text>100</Text>mm
-      </Text>
-      <XStack alignItems="center" space="$1.5">
-        <Checkbox id="scale" defaultChecked={true} borderRadius="100%">
-          <Checkbox.Indicator
-            w="100%"
-            h="100%"
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor={'$primary'}
-            borderRadius="100%"
-          >
-            <Check color="white" />
-          </Checkbox.Indicator>
-        </Checkbox>
-        <Label pt="$2" htmlFor="scale" fontSize="$3">
-          {t('fixed scale')}
-        </Label>
-      </XStack>
-    </XStack>
-  )
-}
-
-const ActionButton: React.FC<
-  GetProps<typeof SRIconButton> & { type?: 's' | 'l' /* active?: boolean */ }
-> = ({ children, icon, type = 'l', /* active, */ ...props }) => {
-  return (
-    <SRIconButton
-      color={'black'}
-      height={'$8'}
-      fontSize={type === 'l' ? '$4' : '$3'}
-      icon={cloneElement(icon as JSX.Element, {
-        // color: active ? '$primary' : '$inactive',
-        color: '$primary',
-        size: type === 'l' ? '$3' : '$2',
-      })}
-      flexDirection="column"
-      {...props}
-    >
-      {children}
-    </SRIconButton>
-  )
-}
-
-const ActionSlider: React.FC<{
-  name: string
-  extra?: React.ReactNode
-}> = ({ name, extra }) => {
-  return (
-    <XStack px={pxSpace} height={'$2.5'} alignItems="center" space="$2" justifyContent="center">
-      <Text>{name}</Text>
-      <Text>50%</Text>
-      <Slider flex={1} defaultValue={[50]} max={100} step={1}>
-        <Slider.Track backgroundColor={'$inactive'}>
-          <Slider.TrackActive backgroundColor={'$primary'} />
-        </Slider.Track>
-        <Slider.Thumb circular index={0} size={'$1'} backgroundColor={'black'} />
-      </Slider>
-      {extra}
-    </XStack>
-  )
-}
+import { ActionButton, ActionStatus } from './actions'
+import { ActionStack, SliderStack } from './operations'
 
 const pxSpace: SpaceTokens = '$3.5'
-
-enum Operation {
-  material,
-  text,
-  brush,
-  album,
-  preview,
-}
 
 /**
  * @description
  * 状态区
  * 画布
- * 辅助动作区
+ * 辅助动作区 Action Stack
  *  - 路径(Path Effect)
- *  - 着色器(Blend Shader)
- * 辅助动作区
+ *  - 颜色滤镜(Color Filter)
+ * 辅助动作区 Slider Stack
  *  - 画笔大小(stroke)
  *  - 对比度(contrast)
+ * @description 设计思路
+ * 每个图层有自己的状态(LayerInfo)
+ * 根据当前选中图层(CurrentLayer)的类型(LayerType)展示配置(Config)
+ * 每个图层消费对应的状态
+ * 根据图层下发的配置展示各个区域
+ * 层级关系 actions -> operation -> carve scene
  */
 export const CarveScene = () => {
   const { t } = useTranslation()
@@ -139,41 +34,13 @@ export const CarveScene = () => {
       <ScrollView>
         <ActionStatus />
         <DrawingBoard ref={drawingBoardRef} />
-        <XStack space="$4" justifyContent="center">
-          <ActionButton active type="s" icon={<MenuSquare />}>
-            {t('solid')}
-          </ActionButton>
-          <ActionButton type="s" icon={<Square />}>
-            {t('hollow')}
-          </ActionButton>
-        </XStack>
-        <XStack space="$4" justifyContent="center">
-          <ActionButton type="s" icon={<Contrast />}>
-            {t('b/w')}
-          </ActionButton>
-          <ActionButton active type="s" icon={<Droplet />}>
-            {t('grayscale')}
-          </ActionButton>
-          <ActionButton type="s" icon={<Lasso />}>
-            {t('outline')}
-          </ActionButton>
-          <ActionButton type="s" icon={<Palette />}>
-            {t('true tone')}
-          </ActionButton>
-        </XStack>
-        <ActionSlider
-          name={t('brush size')}
-          extra={<SRIconButton icon={<Undo2 color="black" size="$1.5" />} />}
-        />
-        <ActionSlider name={t('contrast ratio')} />
-        <ActionSlider name={t('carve precision')} />
-        <ActionSlider name={t('carve speed')} />
-        <ActionSlider name={t('laser power')} />
+        <ActionStack />
+        <SliderStack />
         <XStack px={pxSpace} justifyContent="space-between">
           <ActionButton icon={<Layers />}>{t('material')}</ActionButton>
           <ActionButton icon={<Type />}>{t('text')}</ActionButton>
           <ActionButton icon={<Brush />}>{t('brush')}</ActionButton>
-          <ActionButton onPress={() => drawingBoardRef?.current?.pickImage?.()} active icon={<Image />}>
+          <ActionButton onPress={() => drawingBoardRef?.current?.pickImage?.()} icon={<Image />}>
             {t('album')}
           </ActionButton>
           <ActionButton icon={<View />}>{t('preview')}</ActionButton>
