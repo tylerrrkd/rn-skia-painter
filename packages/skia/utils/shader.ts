968e7b5703b9e21a7e07802ad7c1c5b5fa160f96
Tylerrrkd <kaida23@126.com>
2023-07-23 02:08:34 +0800

    ✨ add image shaders

--- packages/skia/utils/shader.ts
+++ packages/skia/utils/shader.ts
@@ -1,16 +1,48 @@
 import { Skia } from '@shopify/react-native-skia'
 
+/**
+ * @description 重要（texture转换vec3）
+ * vec4 p = image.eval(xy).rgba;
+ * vec3(p.r, p.g, p.b) 相当于 texture(iChannel0, uv).rgb
+ */
+
+export const BLACK_AND_WHITE_SHADER = Skia.RuntimeEffect.Make(`
+uniform shader image;
+
+vec4 main(vec2 xy)
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec4 texColor= image.eval(xy).rgba;
+    
+    // get gray scale or luminescence
+    float gray = 0.33 * (texColor.r + texColor.g + texColor.b);
+    
+    // mix white and black by the value of step(threshold, gray)
+    float threshold = 0.39;
+    if(gray > threshold) {
+      // white
+      texColor.rgb = vec3(1.0);   
+    } else {
+      // black
+      texColor.rgb = vec3(0.0);
+    }
+    
+    // Output to screen
+    return texColor;
+}
+`)!
+
 export const GRAY_SHADER = Skia.RuntimeEffect.Make(`
 uniform shader image;
 
-vec3 greyscale(vec3 color, float str) {
-  float g = dot(color, vec3(0.299, 0.587, 0.114));
-  return mix(color, vec3(g), str) * vec3(1.2, 0, 0);
-}
-
-half4 main(float2 xy) {   
-  vec4 p = image.eval(xy).rgba;
-  vec3 grey = greyscale(vec3(p.r, p.g, p.b), 1.0);
-  return vec4(grey, 1);
+half4 main(in vec2 xy)
+{
+  //Grabbing the texture color at the current pixel.
+  vec4 texColor = image.eval(xy).rgba;
+  
+  vec3 gray_scale = vec3(dot(vec3(0.5, 0.5, 0.5), texColor.rgb));
+      
+  // Output to screen
+  return vec4(gray_scale, texColor.a) ;
 }
 `)!
